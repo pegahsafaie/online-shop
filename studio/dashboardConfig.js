d9d74efee1c7c3d41536c9dfe4fe43c1e62aa220
pegah <pegahsafaie@gmail.com>
2021-11-06 20:19:49 +0100

    modify sanity studio dashboard

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -12,16 +12,10 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '6166c6ec02adb7cac3427717',
-                  title: 'Sanity Studio',
-                  name: 'online-shop-studio',
-                  apiId: 'aa098e07-f528-4483-b362-4aeee4393649'
-                },
-                {
-                  buildHookId: '6166c6ed39d9cff441c5becc',
+                  buildHookId: '61859a33321f9b0084c7469e',
                   title: 'Likörbar',
                   name: 'online-shop-web',
-                  apiId: 'ced071b0-0b91-4e43-8793-dd92b2712d61'
+                  apiId: '7ce67371-3731-4fc7-9227-cef96c0c82b5'
                 }
               ]
             }
@@ -33,15 +27,10 @@ export default {
             value: 'https://github.com/pegahsafaie/online-shop',
             category: 'Code'
           },
-          {title: 'Frontend', value: 'https://online-shop-web.netlify.app', category: 'apps'}
+          {title: 'Frontend', value: 'https://likorbar.netlify.app', category: 'apps'}
         ]
       }
     },
-    {name: 'project-users', layout: {height: 'auto'}},
-    {
-      name: 'document-list',
-      options: {title: 'Newly added products', order: '_createdAt desc', types: ['product']},
-      layout: {width: 'medium'}
-    }
+    {name: 'project-users', layout: {height: 'auto'}}
   ]
 }
